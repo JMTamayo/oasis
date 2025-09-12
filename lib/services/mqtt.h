d9ba99bc36973bd2148230c3ace8a7d202b50f76
JMTamayo <jmtamayog23@gmail.com>
2025-09-12 00:42:24 -0500

    Implement the resource to handle MQTT services

---
+++ lib/services/mqtt.h
@@ -0,0 +1,73 @@
+#ifndef MQTT_H
+#define MQTT_H
+
+#include <PubSubClient.h>
+
+#include "sample.h"
+
+namespace services {
+
+class MqttService {
+private:
+  const char *server;
+  const unsigned int port;
+  const char *user;
+  const char *password;
+  const char *clientId;
+  const unsigned long maxRetryTimeMs;
+
+  const char *topicBase;
+
+  const char *topicMeasurements;
+  const char *topicMeasurementsAir;
+  const char *topicMeasurementsAirTemperature;
+  const char *topicMeasurementsAirHumidity;
+
+  PubSubClient *client;
+
+  const char *getServer() const;
+
+  const unsigned int getPort() const;
+
+  const char *getUser() const;
+
+  const char *getPassword() const;
+
+  const char *getClientId() const;
+
+  const unsigned long getMaxRetryTimeMs() const;
+
+  const char *getTopicBase() const;
+
+  const char *getTopicMeasurements() const;
+
+  const char *getTopicMeasurementsAir() const;
+
+  const char *getTopicMeasurementsAirTemperature() const;
+
+  const char *getTopicMeasurementsAirHumidity() const;
+
+  PubSubClient *getClient();
+
+public:
+  MqttService(const char *server, const unsigned int port, const char *user,
+              const char *password, const char *clientId,
+              const unsigned long maxRetryTimeMs, const char *topicBase,
+              const char *topicMeasurements, const char *topicMeasurementsAir,
+              const char *topicMeasurementsAirTemperature,
+              const char *topicMeasurementsAirHumidity, PubSubClient *client);
+
+  ~MqttService();
+
+  bool IsConnected();
+
+  void Connect();
+
+  void Loop();
+
+  void SendSample(sampling::Sample *sample);
+};
+
+} // namespace services
+
+#endif // MQTT_H
