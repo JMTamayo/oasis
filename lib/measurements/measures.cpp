7e347641ab5c99cf43dceb2cb7855c23aa277fad
JMTamayo <jmtamayog23@gmail.com>
2025-09-14 13:02:18 -0500

    Include pumpState in measurements

--- lib/measurements/measures.cpp
+++ lib/measurements/measures.cpp
@@ -4,14 +4,17 @@
 
 namespace measurements {
 
-Measures::Measures(float airTemperature, float airRelativeHumidity)
-    : airTemperature(airTemperature), airRelativeHumidity(airRelativeHumidity) {
-}
+Measures::Measures(float airTemperature, float airRelativeHumidity,
+                   bool pumpState)
+    : airTemperature(airTemperature), airRelativeHumidity(airRelativeHumidity),
+      pumpState(pumpState) {}
 
 Measures::~Measures() {}
 
 float Measures::GetAirTemperature() { return this->airTemperature; }
 
 float Measures::GetAirRelativeHumidity() { return this->airRelativeHumidity; }
+
+bool Measures::GetPumpState() { return this->pumpState; }
 
 } // namespace measurements
--- lib/measurements/measures.h
+++ lib/measurements/measures.h
@@ -10,14 +10,18 @@ private:
   float airTemperature;
   float airRelativeHumidity;
 
+  bool pumpState;
+
 public:
-  Measures(float airTemperature, float airRelativeHumidity);
+  Measures(float airTemperature, float airRelativeHumidity, bool pumpState);
 
   ~Measures();
 
   float GetAirTemperature();
 
   float GetAirRelativeHumidity();
+
+  bool GetPumpState();
 };
 
 } // namespace measurements
