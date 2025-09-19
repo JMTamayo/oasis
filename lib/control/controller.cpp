10708a2b7daec9889ac7625cd9557a6c34d8f5a5
JMTamayo <jmtamayog23@gmail.com>
2025-09-19 11:23:51 -0500

    Include measures and implement SEN0193 sensor in controller

--- lib/control/controller.cpp
+++ lib/control/controller.cpp
@@ -4,6 +4,28 @@
 
 namespace control {
 
+Measures::Measures(peripherals::AirProperties airProperties,
+                   peripherals::FlowRate waterFlowRate,
+                   peripherals::SoilMoisture soilMoisture, bool pumpState)
+    : airProperties(airProperties), waterFlowRate(waterFlowRate),
+      soilMoisture(soilMoisture), pumpState(pumpState) {}
+
+Measures::~Measures() {}
+
+peripherals::AirProperties Measures::GetAirProperties() {
+  return this->airProperties;
+}
+
+peripherals::FlowRate Measures::GetWaterFlowRate() {
+  return this->waterFlowRate;
+}
+
+peripherals::SoilMoisture Measures::GetSoilMoisture() {
+  return this->soilMoisture;
+}
+
+bool Measures::GetPumpState() { return this->pumpState; }
+
 const unsigned long Controller::getIntervalMs() const {
   return this->intervalMs;
 }
@@ -16,15 +38,18 @@ peripherals::Dht11 *Controller::getDht11() { return this->dht11; }
 
 peripherals::YfS401 *Controller::getYfS401() { return this->yfS401; }
 
+peripherals::Sen0193 *Controller::getSen0193() { return this->sen0193; }
+
 void Controller::setLastMeasurementTimeMs(
     const unsigned long lastMeasurementTimeMs) {
   this->lastMeasurementTimeMs = lastMeasurementTimeMs;
 }
 
 Controller::Controller(const unsigned long intervalMs,
-                       peripherals::Dht11 *dht11, peripherals::YfS401 *yfS401)
+                       peripherals::Dht11 *dht11, peripherals::YfS401 *yfS401,
+                       peripherals::Sen0193 *sen0193)
     : intervalMs(intervalMs), lastMeasurementTimeMs(millis()), dht11(dht11),
-      yfS401(yfS401) {}
+      yfS401(yfS401), sen0193(sen0193) {}
 
 Controller::~Controller() {}
 
@@ -32,17 +57,16 @@ bool Controller::IsMeasurementTimeReached() {
   return millis() - this->getLastMeasurementTimeMs() >= this->getIntervalMs();
 }
 
-measurements::Measures Controller::Measure() {
+Measures Controller::Measure() {
   peripherals::AirProperties airProperties = this->getDht11()->Read();
   peripherals::FlowRate waterFlowRate = this->getYfS401()->Read();
+  peripherals::SoilMoisture soilMoisture = this->getSen0193()->Read();
 
-  bool pumpState = random(0, 2) == 1; // TODO: Implement measure pump state
+  bool pumpState = true; // TODO: Implement measure pump state
 
   this->setLastMeasurementTimeMs(millis());
 
-  return measurements::Measures(
-      airProperties.GetTemperature(), airProperties.GetRelativeHumidity(),
-      waterFlowRate.GetFlowRate(), waterFlowRate.GetVolume(), pumpState);
+  return Measures(airProperties, waterFlowRate, soilMoisture, pumpState);
 }
 
 void Controller::StartPump(bool state) {
