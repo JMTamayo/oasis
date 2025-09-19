4f7170a309ab39f1ef8b75b85e2affc4c9263742
JMTamayo <jmtamayog23@gmail.com>
2025-09-19 11:22:10 -0500

    Implement SEN0193 sensor

---
+++ lib/peripherals/sen0193.cpp
@@ -0,0 +1,86 @@
+#include <Arduino.h>
+
+#include "sen0193.h"
+
+#include "logger.h"
+
+namespace peripherals {
+
+SoilMoisture::SoilMoisture(float moisture, SoilMoistureLevel level)
+    : moisture(moisture), level(level) {}
+
+SoilMoisture::~SoilMoisture() {}
+
+float SoilMoisture::GetMoisture() { return this->moisture; }
+
+SoilMoistureLevel SoilMoisture::GetLevel() { return this->level; }
+
+String SoilMoisture::GetLevelString() {
+  switch (this->level) {
+  case SoilMoistureLevel::EXTREMELY_DRY:
+    return "EXTREMELY_DRY";
+  case SoilMoistureLevel::DRY:
+    return "DRY";
+  case SoilMoistureLevel::MOIST:
+    return "MOIST";
+  case SoilMoistureLevel::WET:
+    return "WET";
+  case SoilMoistureLevel::SATURATED:
+    return "SATURATED";
+  default:
+    return "UNKNOWN";
+  }
+}
+
+const unsigned int Sen0193::getPin() const { return this->pin; }
+
+unsigned long Sen0193::getAirValue() const { return this->airValue; }
+
+unsigned long Sen0193::getWaterValue() const { return this->waterValue; }
+
+Sen0193::Sen0193(const unsigned int pin, unsigned long waterValue,
+                 unsigned long airValue)
+    : pin(pin) {
+  const unsigned long resolution = 4095;
+
+  if (waterValue < airValue && airValue <= resolution) {
+    this->airValue = airValue;
+    this->waterValue = waterValue;
+
+  } else {
+    this->airValue = resolution;
+    this->waterValue = 0;
+  }
+
+  pinMode(pin, INPUT);
+}
+
+Sen0193::~Sen0193() {}
+
+SoilMoisture Sen0193::Read() {
+  SoilMoistureLevel level;
+
+  unsigned long value = analogRead(this->getPin());
+
+  const float airValue = (float)this->getAirValue();
+  const float waterValue = (float)this->getWaterValue();
+  const float range = airValue - waterValue;
+
+  float moisture = 1.0f - ((float)value - waterValue) / range;
+  moisture = max(0.0f, min(1.0f, moisture));
+
+  if (moisture <= 0.2f) {
+    level = SoilMoistureLevel::EXTREMELY_DRY;
+  } else if (moisture <= 0.4f) {
+    level = SoilMoistureLevel::DRY;
+  } else if (moisture <= 0.6f) {
+    level = SoilMoistureLevel::MOIST;
+  } else if (moisture <= 0.8f) {
+    level = SoilMoistureLevel::WET;
+  } else {
+    level = SoilMoistureLevel::SATURATED;
+  }
+
+  return SoilMoisture(moisture * 100.0f, level);
+}
+} // namespace peripherals
---
+++ lib/peripherals/sen0193.h
@@ -0,0 +1,55 @@
+#ifndef SEN0193_H
+#define SEN0193_H
+
+namespace peripherals {
+
+enum SoilMoistureLevel {
+  EXTREMELY_DRY = 0,
+  DRY = 1,
+  MOIST = 2,
+  WET = 3,
+  SATURATED = 4
+};
+
+class SoilMoisture {
+private:
+  float moisture;
+  SoilMoistureLevel level;
+
+public:
+  SoilMoisture(float moisture, SoilMoistureLevel level);
+
+  ~SoilMoisture();
+
+  float GetMoisture();
+
+  SoilMoistureLevel GetLevel();
+
+  String GetLevelString();
+};
+
+class Sen0193 {
+private:
+  const unsigned int pin;
+
+  unsigned long airValue;
+  unsigned long waterValue;
+
+  const unsigned int getPin() const;
+
+  unsigned long getAirValue() const;
+
+  unsigned long getWaterValue() const;
+
+public:
+  Sen0193(const unsigned int pin, unsigned long waterValue,
+          unsigned long airValue);
+
+  ~Sen0193();
+
+  SoilMoisture Read();
+};
+
+} // namespace peripherals
+
+#endif // SEN0193_H
