2d8dfef0577aa2cdda35586dc2217a52a99f6a62
JMTamayo <jmtamayog23@gmail.com>
2025-10-11 21:55:03 -0500

    Include required parameters to handle LCD1602I2C, low moisture led indicator and rename the MQTT topic values

--- include/parameters.h
+++ include/parameters.h
@@ -1,35 +1,30 @@
+// Configuration parameters to handle WiFi, MQTT and geolocation services:
 #define WIFI_MAX_RETRY_TIME_MILLISECONDS 5000
-
 #define MQTT_MAX_RETRY_TIME_MILLISECONDS 5000
+#define GEOLOCATION_REQUEST_TIMEOUT_MILLISECONDS 500
 
-#define GEOLOCATION_REQUEST_TIMEOUT_MILLISECONDS 1000
-#define GEOLOCATION_INTERVAL_MILLISECONDS 10000
-
+// Configuration parameters to handle MQTT topics:
 #define MQTT_TOPIC_BASE "/oasis"
+#define MQTT_TOPIC_AIR_TEMPERATURE "/air-temperature"
+#define MQTT_TOPIC_AIR_RELATIVE_HUMIDITY "/air-relative-humidity"
+#define MQTT_TOPIC_SOIL_MOISTURE "/soil-moisture"
+#define MQTT_TOPIC_GEOLOCATION "/location"
+#define MQTT_TOPIC_CONTROL_PUMP "/control-pump"
 
-#define MQTT_TOPIC_MEASUREMENTS "/measurements"
-#define MQTT_TOPIC_MEASUREMENTS_AIR "/air"
-#define MQTT_TOPIC_MEASUREMENTS_AIR_TEMPERATURE "/temperature"
-#define MQTT_TOPIC_MEASUREMENTS_AIR_RELATIVE_HUMIDITY "/relativeHumidity"
-#define MQTT_TOPIC_MEASUREMENTS_WATER "/water"
-#define MQTT_TOPIC_MEASUREMENTS_WATER_FLOW_RATE "/flowRate"
-#define MQTT_TOPIC_MEASUREMENTS_SOIL "/soil"
-#define MQTT_TOPIC_MEASUREMENTS_SOIL_MOISTURE "/moisture"
-#define MQTT_TOPIC_MEASUREMENTS_SOIL_MOISTURE_LEVEL "/moistureLevel"
-#define MQTT_TOPIC_MEASUREMENTS_PUMP "/pump"
-#define MQTT_TOPIC_MEASUREMENTS_PUMP_STATE "/state"
-#define MQTT_TOPIC_MEASUREMENTS_GEOLOCATION "/geolocation"
-
-#define MQTT_TOPIC_CONTROL "/control"
-#define MQTT_TOPIC_CONTROL_PUMP "/pump"
-
+// Configuration parameters to handle measuring:
 #define MEASURING_INTERVAL_MILLISECONDS 2000
+#define GEOLOCATION_INTERVAL_MILLISECONDS 60000
 
-#define DHT11_PIN 32
+// Configuration parameters to handle DHT22:
+#define DHT22_PIN 32
 
-#define YF_S401_PIN 35
-#define YF_S401_VOLUME_PER_PULSE 0.17006803 // milliliters per pulse
+// Configuration parameters to handle LCD1602I2C:
+#define LCD1602I2C_ADDRESS 0x27
+#define LCD1602I2C_COLS 16
+#define LCD1602I2C_ROWS 2
 
-#define SEN0193_PIN 34
-#define SEN0193_WATER_VALUE 800
-#define SEN0193_AIR_VALUE 3500
+// Configuration parameters to handle SEN0193:
+#define SEN0193_PIN 35
+#define SEN0193_WATER_VALUE 1250
+#define SEN0193_AIR_VALUE 3300
+#define LOW_MOISTURE_LED_INDICATOR_PIN 18
