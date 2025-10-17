0f4eaa0c81449ab834d4fe15db1fbdc94bffc07e
JMTamayo <jmtamayog23@gmail.com>
2025-10-16 21:12:15 -0500

    Implement last controller version in main loop

--- src/main.cpp
+++ src/main.cpp
@@ -35,11 +35,23 @@ String TOPIC_AIR_RELATIVE_HUMIDITY =
 String TOPIC_SOIL_MOISTURE =
     String(MQTT_TOPIC_BASE) + String(MQTT_TOPIC_SOIL_MOISTURE);
 
+String TOPIC_TANK_LEVEL =
+    String(MQTT_TOPIC_BASE) + String(MQTT_TOPIC_TANK_LEVEL);
+
 String TOPIC_GEOLOCATION =
     String(MQTT_TOPIC_BASE) + String(MQTT_TOPIC_GEOLOCATION);
 
 String TOPIC_CONTROL_PUMP =
     String(MQTT_TOPIC_BASE) + String(MQTT_TOPIC_CONTROL_PUMP);
+
+String TOPIC_CONTROL_DISPLAY_BACKLIGHT =
+    String(MQTT_TOPIC_BASE) + String(MQTT_TOPIC_CONTROL_DISPLAY_BACKLIGHT);
+
+String TOPIC_CONTROL_TAKE_MEASUREMENTS =
+    String(MQTT_TOPIC_BASE) + String(MQTT_TOPIC_CONTROL_TAKE_MEASUREMENTS);
+
+String TOPIC_CONTROL_RESET_DEVICE =
+    String(MQTT_TOPIC_BASE) + String(MQTT_TOPIC_CONTROL_RESET_DEVICE);
 
 void mqttSubscriptionCallback(char *topic, byte *payload, unsigned int length) {
   String topicStr = topic;
@@ -49,16 +61,35 @@ void mqttSubscriptionCallback(char *topic, byte *payload, unsigned int length) {
   services::MqttMessage *mqttMessage =
       new services::MqttMessage(topicStr, payloadString);
 
-  if (topicStr == TOPIC_CONTROL_PUMP &&
-      (payloadString == "0" || payloadString == "1")) {
+  if (topicStr.equals(TOPIC_CONTROL_PUMP) &&
+      (payloadString.equals("0") || payloadString.equals("1"))) {
     logging::logger->Debug("Received message from MQTT server. Topic: " +
                            topicStr + ". Payload: " + payloadString);
-    xQueueSend(MqttSubscriptionsEventQueue, &mqttMessage, pdMS_TO_TICKS(200));
+
+  } else if (topicStr.equals(TOPIC_CONTROL_DISPLAY_BACKLIGHT) &&
+             (payloadString.equals("0") || payloadString.equals("1"))) {
+    logging::logger->Debug("Received message from MQTT server. Topic: " +
+                           topicStr + ". Payload: " + payloadString);
+
+  } else if (topicStr.equals(TOPIC_CONTROL_TAKE_MEASUREMENTS) &&
+             payloadString.equals("1")) {
+    logging::logger->Debug("Received message from MQTT server. Topic: " +
+                           topicStr + ". Payload: " + payloadString);
+
+  } else if (topicStr.equals(TOPIC_CONTROL_RESET_DEVICE) &&
+             payloadString.equals("1")) {
+    logging::logger->Debug("Received message from MQTT server. Topic: " +
+                           topicStr + ". Payload: " + payloadString);
+
   } else {
     logging::logger->Debug(
         "Received unimplemented message from MQTT server. Topic: " + topicStr +
         ". Payload: " + payloadString + ". Ignoring message.");
+
+    return;
   }
+
+  xQueueSend(MqttSubscriptionsEventQueue, &mqttMessage, pdMS_TO_TICKS(200));
 }
 
 void serverHandling(void *parameter) {
@@ -72,6 +103,7 @@ void serverHandling(void *parameter) {
   services::MqttService *mqtt = new services::MqttService(
       MQTT_SERVER, MQTT_PORT, MQTT_USER, MQTT_PASSWORD, MQTT_CLIENT_ID,
       MQTT_MAX_RETRY_TIME_MILLISECONDS, mqttClient);
+
   services::MqttMessage *mqttMessage;
 
   while (true) {
@@ -79,8 +111,12 @@ void serverHandling(void *parameter) {
       wifi->Connect();
 
     } else if (!mqtt->IsConnected()) {
-      if (mqtt->Connect())
-        mqtt->Subscribe((TOPIC_CONTROL_PUMP).c_str());
+      if (mqtt->Connect()) {
+        mqtt->Subscribe(TOPIC_CONTROL_PUMP);
+        mqtt->Subscribe(TOPIC_CONTROL_DISPLAY_BACKLIGHT);
+        mqtt->Subscribe(TOPIC_CONTROL_TAKE_MEASUREMENTS);
+        mqtt->Subscribe(TOPIC_CONTROL_RESET_DEVICE);
+      }
 
     } else {
       mqtt->Loop();
@@ -111,13 +147,18 @@ void geolocationHandling(void *parameter) {
                    pdMS_TO_TICKS(10));
       }
     }
+
     vTaskDelay(pdMS_TO_TICKS(100));
   }
 }
 
 void controlHandling(void *parameter) {
   peripherals::Dht22 *dht22 = new peripherals::Dht22(DHT22_PIN);
-  vTaskDelay(pdMS_TO_TICKS(2000));
+
+  peripherals::Hcsr04 *hcsr04 = new peripherals::Hcsr04(
+      HCSR04_PIN_ECHO, HCSR04_PIN_TRIGGER, HCSR04_SENSOR_HEIGHT,
+      HCSR04_MAX_LEVEL_HEIGHT, HCSR04_SIGNAL_DELAY_MICROSECONDS,
+      HCSR04_SOUND_SPEED_CM_PER_MICROSECOND);
 
   peripherals::Lcd1602I2c *lcd1602I2c = new peripherals::Lcd1602I2c(
       LCD1602I2C_ADDRESS, LCD1602I2C_COLS, LCD1602I2C_ROWS);
@@ -125,33 +166,45 @@ void controlHandling(void *parameter) {
   peripherals::Sen0193 *sen0193 = new peripherals::Sen0193(
       SEN0193_PIN, SEN0193_WATER_VALUE, SEN0193_AIR_VALUE);
 
-  peripherals::Led *lowMoistureLedIndicator =
-      new peripherals::Led(LOW_MOISTURE_LED_INDICATOR_PIN);
+  peripherals::Led *warningLedIndicator =
+      new peripherals::Led(WARNING_LED_INDICATOR_PIN);
 
   control::Controller *controller =
-      new control::Controller(MEASURING_INTERVAL_MILLISECONDS, dht22, sen0193,
-                              lowMoistureLedIndicator, lcd1602I2c);
+      new control::Controller(MEASURING_INTERVAL_MILLISECONDS, dht22, hcsr04,
+                              sen0193, warningLedIndicator, lcd1602I2c);
 
   services::MqttMessage *mqttMessage;
 
-  while (true) {
-    controller->Loop();
+  control::Measures *measures = nullptr;
+  bool takeMeasurements = false;
 
+  while (true) {
     if (xQueueReceive(MqttSubscriptionsEventQueue, &mqttMessage,
                       pdMS_TO_TICKS(10)) == pdPASS) {
-      if (String(mqttMessage->GetTopic()) == TOPIC_CONTROL_PUMP) {
-        bool pumpState = String(mqttMessage->GetPayload()) == "1";
-        logging::logger->Debug("Processing pump control request. Operation: " +
-                               String(pumpState ? "ON" : "OFF"));
-        controller->StartPump(pumpState);
+      if (mqttMessage->GetTopic().equals(TOPIC_CONTROL_PUMP)) {
+        controller->StartPump(mqttMessage->GetPayload().equals("1"));
+
+      } else if (mqttMessage->GetTopic().equals(
+                     TOPIC_CONTROL_DISPLAY_BACKLIGHT)) {
+        controller->DisplayBacklightOn(mqttMessage->GetPayload().equals("1"));
+
+      } else if (mqttMessage->GetTopic().equals(
+                     TOPIC_CONTROL_TAKE_MEASUREMENTS)) {
+        takeMeasurements = mqttMessage->GetPayload().equals("1");
+
+      } else if (mqttMessage->GetTopic().equals(TOPIC_CONTROL_RESET_DEVICE)) {
+        if (mqttMessage->GetPayload().equals("1"))
+          ESP.restart();
       }
+
       delete mqttMessage;
     }
 
-    if (controller->IsMeasurementTimeReached()) {
-      control::Measures measure = controller->Measure();
+    controller->Loop();
+    measures = controller->Measure(takeMeasurements);
 
-      float airTemperature = measure.GetAirTemperature();
+    if (measures != nullptr) {
+      float airTemperature = measures->GetAirTemperature();
       if (!isnan(airTemperature)) {
         services::MqttMessage *airTemperatureMessage =
             new services::MqttMessage(TOPIC_AIR_TEMPERATURE, airTemperature);
@@ -159,7 +212,7 @@ void controlHandling(void *parameter) {
                    pdMS_TO_TICKS(10));
       }
 
-      float airRelativeHumidity = measure.GetAirRelativeHumidity();
+      float airRelativeHumidity = measures->GetAirRelativeHumidity();
       if (!isnan(airRelativeHumidity)) {
         services::MqttMessage *airRelativeHumidityMessage =
             new services::MqttMessage(TOPIC_AIR_RELATIVE_HUMIDITY,
@@ -168,7 +221,7 @@ void controlHandling(void *parameter) {
                    pdMS_TO_TICKS(10));
       }
 
-      float soilMoisture = measure.GetSoilMoisture();
+      float soilMoisture = measures->GetSoilMoisture();
       if (!isnan(soilMoisture)) {
         services::MqttMessage *soilMoistureMessage =
             new services::MqttMessage(TOPIC_SOIL_MOISTURE, soilMoisture);
@@ -176,8 +229,19 @@ void controlHandling(void *parameter) {
                    pdMS_TO_TICKS(10));
       }
 
-      vTaskDelay(pdMS_TO_TICKS(100));
+      float tankLevel = measures->GetTankLevel();
+      if (!isnan(tankLevel)) {
+        services::MqttMessage *tankLevelMessage =
+            new services::MqttMessage(TOPIC_TANK_LEVEL, tankLevel);
+        xQueueSend(MqttPublishingEventQueue, &tankLevelMessage,
+                   pdMS_TO_TICKS(10));
+      }
+
+      takeMeasurements = false;
+      delete measures;
     }
+
+    vTaskDelay(pdMS_TO_TICKS(100));
   }
 }
 
