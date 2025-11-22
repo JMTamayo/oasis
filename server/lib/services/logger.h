2d710da098fe1c1c23298a7de7be6aa550de02f3
JMTamayo <jmtamayog23@gmail.com>
2025-11-22 13:55:20 -0500

    (server) Implement the module to manage logging

---
+++ server/lib/services/logger.h
@@ -0,0 +1,32 @@
+#ifndef LOGGER_H
+#define LOGGER_H
+
+#include <Arduino.h>
+
+namespace logging {
+
+class Logger {
+private:
+  const unsigned long baudRate;
+
+  const unsigned long getBaudRate() const;
+
+public:
+  Logger();
+
+  ~Logger();
+
+  void Begin();
+
+  void Error(String message);
+
+  void Warning(String message);
+
+  void Debug(String message);
+
+  void Info(String message);
+};
+
+} // namespace logging
+
+#endif // LOGGER_H
