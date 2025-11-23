0b95de28ed20c6a8c161f582df136c024340b8a9
JMTamayo <jmtamayog23@gmail.com>
2025-11-23 15:40:49 -0500

    (server) Implement the module to manage LEDs in ON and OFF mode

---
+++ controller/lib/peripherals/led.cpp
@@ -0,0 +1,13 @@
+#include "led.h"
+
+namespace peripherals {
+
+const unsigned int Led::getPin() const { return this->pin; }
+
+Led::Led(unsigned int pin) : pin(pin) { pinMode(this->getPin(), OUTPUT); }
+
+Led::~Led() {}
+
+void Led::Light(bool on) { digitalWrite(this->getPin(), on ? HIGH : LOW); }
+
+} // namespace peripherals
